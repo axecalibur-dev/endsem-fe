7cc392613d7a59f9c7ecd60eae0f3915f4c7b17a
Jai Bhatt <jaibhatt@Jais-MacBook-Pro-14.local>
2024-12-10 11:31:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';  // Import global styles
+import Header from './components/header';
+import Hero from './components/hero';
+
+function App() {
+  return (
+      <div className="App">
+        <Header />
+        <Hero />
+      </div>
+  );
+}
+
+export default App;
